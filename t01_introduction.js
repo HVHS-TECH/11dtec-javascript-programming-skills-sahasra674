9351b8ba82122902989b765b732af013c7029797
sahasra674 <23409sg@hvhs.school.nz>
2025-04-28 21:54:37 +0000

    Apr 28, 2025, 9:54 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,7 @@
 /************************************* 
-task 1 introduction.js sahasra 29/4/25 
+task 1 introduction.js 
+sahasra 
+29/4/25 
 **************************************/
 console.log("hi.js");
 
