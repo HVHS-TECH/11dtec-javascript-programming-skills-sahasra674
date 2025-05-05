0c10b9e1254ef9bcb7c45b387f41643869efaa65
sahasra674 <23409sg@hvhs.school.nz>
2025-05-05 21:38:57 +0000

    May 5, 2025, 9:38 PM

---
+++ t03_maths.js
@@ -0,0 +1,25 @@
+/************************************* 
+task 2 variables.js 
+sahasra 
+29/4/25 
+**************************************/
+console.log("variables.js");
+
+//variables
+let name ="sahasra"
+let age=15
+let pocketMoney=40
+let year=2025
+
+/******** 
+main code
+********/
+
+console.log("hi" , name)
+console.log("as of" , year , "you are" , age , "years old")
+console.log("you have" , pocketMoney, "dollars")
+
+
+/******** 
+functions
+********/
