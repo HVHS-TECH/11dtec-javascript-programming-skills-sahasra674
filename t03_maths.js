7e5026abab889feacfe0b139626fd610d0016ca1
sahasra674 <23409sg@hvhs.school.nz>
2025-05-05 21:55:35 +0000

    May 5, 2025, 9:55 PM

--- t03_maths.js
+++ t03_maths.js
@@ -3,13 +3,14 @@ task 2 variables.js
 sahasra 
 29/4/25 
 **************************************/
-console.log("variables.js");
+console.log("maths.js");
 
 //variables
 let name ="Sahasra"
 let age=15
 let pocketMoney= 40
 let year=2025
+let birthMonth= 2010
 var myNumber = 15
 var myOtherNumber = 2
 /******** 
@@ -18,6 +19,7 @@ main code
 
 console.log("hi" , name)
 console.log("as of" , year , "you are" , age , "years old")
+console.log("you were born in" , birthMonth )
 console.log("you have" , pocketMoney, "dollars")
 console.log("in 10 years you will be" , myNumber+ 10 , "years old")
 console.log("you have" , pocketMoney, "dollars" )
