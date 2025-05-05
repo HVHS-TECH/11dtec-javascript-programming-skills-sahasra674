5f84763cbc73f5aa76a7efd92d2bada05ed25443
sahasra674 <23409sg@hvhs.school.nz>
2025-05-05 21:45:30 +0000

    May 5, 2025, 9:45 PM

--- t03_maths.js
+++ t03_maths.js
@@ -11,6 +11,7 @@ let age=15
 let pocketMoney=40
 let year=2025
 var myNumber = 15
+
 /******** 
 main code
 ********/
@@ -18,8 +19,9 @@ main code
 console.log("hi" , name)
 console.log("as of" , year , "you are" , age , "years old")
 console.log("you have" , pocketMoney, "dollars")
-console.log()
-
+console.log("in 10 years you will be" , myNumber+ 10 , "years old")
+console.log("you have" , pocketMoney, "dollars" )
+console.log("half of you money is" )
 /******** 
 functions
 ********/
