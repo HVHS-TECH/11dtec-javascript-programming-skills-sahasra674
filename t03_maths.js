ea8151be8ba36d2b68681e84027e523a79a37ac9
sahasra674 <23409sg@hvhs.school.nz>
2025-05-09 03:15:16 +0000

    3

--- t03_maths.js
+++ t03_maths.js
@@ -7,23 +7,27 @@ console.log("maths.js");
 
 //variables
 let name ="Sahasra"
-let age=15
+let age=100
 let pocketMoney= 40
 let year=2025
-let birthMonth= 2010
-var myNumber = 15
-var myOtherNumber = 2
+let birthYear= year -age
+//var tenYearsOlder = +10
+//var halfMoney= /2
 /******** 
 main code
 ********/
+console.log("as of" , year , "you are" , age , "years old")
+console.log("you were born in" , birthYear)
 
+/*
 console.log("hi" , name)
 console.log("as of" , year , "you are" , age , "years old")
-console.log("you were born in" , birthMonth )
+console.log("you were born in" , birthYear)
 console.log("you have" , pocketMoney, "dollars")
 console.log("in 10 years you will be" , myNumber+ 10 , "years old")
 console.log("you have" , pocketMoney, "dollars" )
 console.log("half of you money is" , pocketMoney/myOtherNumber )
+*/
 /******** 
 functions
 ********/
---
+++ t05_alerts_prompts.js
@@ -0,0 +1,30 @@
+/************************************* 
+task 3 maths.js 
+sahasra 
+29/4/25 
+**************************************/
+console.log("maths.js");
+
+//variables
+let name ="Sahasra"
+let age=100
+let pocketMoney= 40
+let year=2025
+let birthMonth= 2010
+var myNumber = 15
+var myOtherNumber = 2
+/******** 
+main code
+********/
+
+console.log("hi" , name)
+console.log("as of" , year , "you are" , age , "years old")
+console.log("you were born in" , birthMonth )
+console.log("you have" , pocketMoney, "dollars")
+console.log("in 10 years you will be" , myNumber+ 10 , "years old")
+console.log("you have" , pocketMoney, "dollars" )
+console.log("half of you money is" , pocketMoney/myOtherNumber )
+prompt()
+/******** 
+functions
+********/
