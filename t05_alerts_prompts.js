d38e58b3861f25c3d4d197b53e1c4992a2c966d3
sahasra674 <23409sg@hvhs.school.nz>
2025-05-12 21:48:36 +0000

    May 12, 2025, 9:48 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -23,7 +23,7 @@ console.log("you have" , pocketMoney, "dollars")
 console.log("in 10 years you will be" , myNumber+ 10 , "years old")
 console.log("you have" , pocketMoney, "dollars" )
 console.log("half of you money is" , pocketMoney/myOtherNumber )
- var username=prompt("what is your name?", sahasra)
+var username=prompt("what is your name?")
 /******** 
 functions
 ********/
