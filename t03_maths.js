99aa620078adeac42a8707f8c619f19b6a5eb87e
sahasra674 <23409sg@hvhs.school.nz>
2025-05-12 21:28:32 +0000

    May 12, 2025, 9:28 PM

--- t03_maths.js
+++ t03_maths.js
@@ -7,10 +7,10 @@ console.log("maths.js");
 
 //variables
 let name ="Sahasra"
-let age=100
+let age= 100
 let pocketMoney= 40
-let year=2025
-let birthYear= year -age
+let year= 2025
+let birthYear= year-age
 //var tenYearsOlder = +10
 //var halfMoney= /2
 /******** 
@@ -18,16 +18,14 @@ main code
 ********/
 console.log("as of" , year , "you are" , age , "years old")
 console.log("you were born in" , birthYear)
-
-/*
 console.log("hi" , name)
 console.log("as of" , year , "you are" , age , "years old")
 console.log("you were born in" , birthYear)
 console.log("you have" , pocketMoney, "dollars")
-console.log("in 10 years you will be" , myNumber+ 10 , "years old")
+console.log("in 10 years you will be" , birthYear , "years old")
 console.log("you have" , pocketMoney, "dollars" )
 console.log("half of you money is" , pocketMoney/myOtherNumber )
-*/
+
 /******** 
 functions
 ********/
