2c9c0b43e8a54ca830de93cc76be699b1233cf78
sahasra674 <23409sg@hvhs.school.nz>
2025-05-12 21:20:45 +0000

    May 12, 2025, 9:20 PM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -10,16 +10,13 @@ let name ="Sahasra"
 let age=100
 let pocketMoney= 40
 let year=2025
-let birthMonth= 2010
+let birthYear= year-age
 var myNumber = 15
 var myOtherNumber = 2
 /******** 
 main code
 ********/
-console.log("as of" , year , "you are" , age , "years old")
-console.log("you were born in" , birthMonth )
 
-/*
 console.log("hi" , name)
 console.log("as of" , year , "you are" , age , "years old")
 console.log("you were born in" , birthMonth )
@@ -28,8 +25,6 @@ console.log("in 10 years you will be" , myNumber+ 10 , "years old")
 console.log("you have" , pocketMoney, "dollars" )
 console.log("half of you money is" , pocketMoney/myOtherNumber )
 prompt()
-*/
-
 /******** 
 functions
 ********/
