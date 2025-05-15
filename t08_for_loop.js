62a0de60b12e19f3af6216cc9f128668429add0f
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:00:54 +0000

    May 15, 2025, 6:00 AM

---
+++ t08_for_loop.js
@@ -0,0 +1,13 @@
+/************************************* 
+task 1 introduction.js 
+sahasra 
+29/4/25 
+**************************************/
+console.log("for loop.js");
+
+//variables
+
+
+/******** 
+main code
+********/
