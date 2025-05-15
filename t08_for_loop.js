1f66779d9b7fa505f80d7e4b2cb13f8c09a134c5
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:20:22 +0000

    May 15, 2025, 6:20 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -1,15 +1,20 @@
 /************************************* 
- task 1 introduction.js 
- sahasra 
- 29/4/25 
+task 1 introduction.js 
+sahasra 
+29/4/25 
 **************************************/
-console.log("introduction.js");
+console.log("hi.js");
 
-// Main code
-alert("Welcome to the program!");
+//variables
 
-let numberOne = Number(prompt("What is your 1st number?"));
-let numberTwo = Number(prompt("What is your 2nd number?"));
-let numberThree = Number(prompt("What is your 3rd number?"));
-let numberFour = Number(prompt("What is your 4th number?"));
-let numberFive = Number(prompt("What is your 5th number?"));
+
+/******** 
+main code
+********/
+
+
+
+
+/******** 
+functions
+********/
