aedb68b3d67ff1fd312ea9745d7f65a58c90d975
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:39:35 +0000

    May 15, 2025, 6:39 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -1,13 +1,21 @@
-console.log("for_loop.js");
+<!DOCTYPE html>
+<html>
+<head>
+  <title>For Loop Total</title>
+</head>
+<body>
+  <script>
+    alert("Welcome to the program!");
 
-let number1 = 0;
-let number2= 0;
-let number3= 0;
-let number4= 0;
-let number5= 0;
+    let total = 0;
+    let number = 0;
 
-var number=prompt("what is your 1st number?")
-var number =prompt("what is your 2nd number?")
-var number =prompt("what is your 3rd number?")
-var number=prompt("what is your 4th number?")
-var number=prompt("what is your 5th number?")
+    for (let count = 0; count < 5; count++) {
+      number = Number(prompt("Enter a number:"));
+      total = total + number;
+    }
+
+    alert("The total is: " + total);
+  </script>
+</body>
+</html>
