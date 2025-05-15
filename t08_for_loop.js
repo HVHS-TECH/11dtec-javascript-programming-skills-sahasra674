b38d30be22068cf17d61593abe68c01cd64a8773
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:09:25 +0000

    May 15, 2025, 6:09 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -15,10 +15,12 @@ var numberFive= 5;
 
 /******** 
 main code
+*******/
 alert("welcome to the program!")
 var 1=prompt("what is your 1st number?")
 var 2=prompt("what is you 2nd number?")
 var 3=prompt("what is your 3rd number?")
 var 4= prompt("what is your 4th number?")
 var 5= prompt("what is your 5th number?")
-********/
+
+
