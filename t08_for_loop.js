887d76475f3975c6beb44ed3daa493c0d165985c
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:31:24 +0000

    May 15, 2025, 6:31 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -1,4 +1,13 @@
-let name ="Sahasra"
-let age=15
-let pocketMoney=40
-let year=2025
+console.log("for_loop.js");
+
+let number1 = 0;
+let number2= 0;
+let number3= 0;
+let number4= 0;
+let number5= 0;
+
+var number=prompt("what is your 1st number?")
+var number =prompt("what is your 2nd number?")
+var number =prompt("what is your 3rd number?")
+var number=prompt("what is your 4th number?")
+var number=prompt("what is your 5th number?")
