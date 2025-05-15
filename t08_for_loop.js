2225c1289692f34fed8667cc4473354ff381b0da
sahasra674 <23409sg@hvhs.school.nz>
2025-05-15 06:12:07 +0000

    May 15, 2025, 6:12 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -6,21 +6,21 @@ sahasra
 console.log("for loop.js");
 
 //variables
-var numberOne= 1;
-var numberTwo= 2;
-var numberThree= 3;
-var numberFour= 4;
-var numberFive= 5;
+var numberOne= 0-5;
+var numberTwo= 0-5;
+var numberThree= 0-5;
+var numberFour= 0-5;
+var numberFive= 0-5;
 
 
 /******** 
 main code
 *******/
 alert("welcome to the program!")
-var 1=prompt("what is your 1st number?")
-var 2=prompt("what is you 2nd number?")
-var 3=prompt("what is your 3rd number?")
-var 4= prompt("what is your 4th number?")
-var 5= prompt("what is your 5th number?")
+var numberOne=prompt("what is your 1st number?")
+var numberTwo=prompt("what is you 2nd number?")
+var numberThree=prompt("what is your 3rd number?")
+var numberFour= prompt("what is your 4th number?")
+var numberFive= prompt("what is your 5th number?")
 
 
