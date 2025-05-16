35e6f10d7b9f21d1cfb2cfa1c90e4b86652f84d4
sahasra674 <23409sg@hvhs.school.nz>
2025-05-16 02:30:36 +0000

    May 16, 2025, 2:30 AM

---
+++ t08_for_loop.js
@@ -0,0 +1,22 @@
+/************************************* 
+task 1 introduction.js 
+sahasra 
+29/4/25 
+**************************************/
+console.log("hi.js");
+
+//variables
+let numberOne= 1
+let numberTwo= 2
+let numberThree= 3
+
+/******** 
+main code
+********/
+
+for(count=3; count<20; count++)
+
+
+/******** 
+functions
+********/
