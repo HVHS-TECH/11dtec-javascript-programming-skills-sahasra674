6b8a4802aeecfd5939e34cd54ce71579fcc371bd
sahasra674 <23409sg@hvhs.school.nz>
2025-05-16 02:49:21 +0000

    May 16, 2025, 2:49 AM

---
+++ t09_functions.js
@@ -0,0 +1,23 @@
+/************************************* 
+task 1 introduction.js 
+sahasra 
+29/4/25 
+**************************************/
+console.log("hi.js");
+
+//variables
+let numberOne= 1
+let numberTwo= 2
+let numberThree= 3
+
+/******** 
+main code
+********/
+console.log("starting loop")
+for(count=0; count < 20; count++);{
+console.log("current count :" + count);
+}
+console.log("loop stopped")
+/******** 
+functions
+********/
