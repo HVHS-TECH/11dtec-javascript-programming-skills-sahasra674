c50969dc2a3fb21fad5eaf677ef4f29227fa307b
sahasra674 <23409sg@hvhs.school.nz>
2025-05-27 23:55:03 +0000

    May 27, 2025, 11:55 PM

--- t12_while_loop.js
+++ t12_while_loop.js
@@ -1,5 +1,5 @@
 var password= prompt("speak 'friend' and enter");
 while (password !="friend"){
-    password= prompt("that's not the password.\n speak 'friend' and enter");
+    password= prompt("that's not the password.\nspeak 'friend' and enter");
 }
 alert("welcome friend!")
